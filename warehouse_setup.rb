49ea36518d648a1214aed50375aac64b754c93c1
Derek George <derek68038@gmail.com>
2015-02-06 16:49:24 -0600

    Program works. Still needs documentation, testing, & possible refactoring.

--- warehouse_setup.rb
+++ warehouse_setup.rb
@@ -1,12 +1,12 @@
 # Get results as hash
 DB.results_as_hash = true
 
-DB.execute("CREATE TABLE IF NOT EXISTS locations (location_id INTEGER PRIMARY 
+DB.execute("CREATE TABLE IF NOT EXISTS locations (id INTEGER PRIMARY 
           KEY, city TEXT)")
 
 DB.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,
-          serial_number INTEGER, quantity INTEGER, description TEXT, cost
+          serial_number INTEGER, description TEXT, quantity INTEGER, cost
           INTEGER, location_id INTEGER, category_id INTEGER)")
 
-DB.execute("CREATE TABLE IF NOT EXISTS categories (category_id INTEGER PRIMARY
+DB.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY
           KEY, name TEXT)")
