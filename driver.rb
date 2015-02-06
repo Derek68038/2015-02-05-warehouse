eaab1901da6a2bab4b7e89322defec03f4c28016
Derek George <derek68038@gmail.com>
2015-02-06 12:42:39 -0600

    Empowered my program with SQLite.  Loaded/created database for this program.

---
+++ driver.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+# Empower my program with SQLite.
+require "sqlite3"
+
+# Load/create our database for this program.
+DB = SQLite3::Database.new("warehouse_mgr.db")
+
+require_relative "location.rb"
+require_relative "category.rb"
+require_relative "product.rb"
+require_relative "warehouse_setup.rb"
+
+
+binding.pry
