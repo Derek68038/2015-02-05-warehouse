98defae8fc158575438fd4b561422ad1a091aa7d
Derek George <derek68038@gmail.com>
2015-02-08 10:33:02 -0600

    Created a test for the .all method which tests all 3 classes.
    Created a test for the .find method which tests all 3 classes.
    
    All tests pass.

--- test_file.rb
+++ test_file.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 require 'minitest/autorun'
 
 require "sqlite3"
@@ -9,8 +9,11 @@ require_relative "warehouse_setup.rb"
 require_relative 'product.rb'
 require_relative 'location.rb'
 require_relative 'category.rb'
+require_relative 'warehouse_methods.rb'
 
 class WarehouseTest < Minitest::Test
+  
+  include WarehouseMethods
   
   def setup
     DATABASE.execute("DELETE FROM products")
@@ -18,49 +21,80 @@ class WarehouseTest < Minitest::Test
     DATABASE.execute("DELETE FROM categories")
   end
   
+  def test_product_creation
+    product = Product.new({"serial_number" => 1, "description" => "Hello", 
+                            "quantity" => 5, "cost" => 10, "location_id" => 1,
+                            "category_id" => 3})
+    
+    product.insert("products")   
+    results = DATABASE.execute("SELECT description FROM products WHERE id = 
+                                #{product.id}")
+    
+    added_product = results[0]
+    
+    assert_equal("Hello", added_product["description"])
+  end
+  
   def test_location_creation
     location = Location.new({"city" => "Omaha"})
-    location.insert   
-    results = DATABASE.execute("SELECT city FROM locations WHERE id = #{location.id}")
+    
+    location.insert("locations")   
+    results = DATABASE.execute("SELECT city FROM locations WHERE id = 
+                                #{location.id}")
     
     added_location = results[0]
     
-    assert_equal(1, results.length)
     assert_equal("Omaha", added_location["city"])
-  end
-  
-  def test_list_all_locations
-    DATABASE.execute("DELETE FROM locations")
-    
-    l1 = Location.new({"city" => "Omaha"})
-    l2 = Location.new({"city" => "Lincoln"})
-    l3 = Location.new({"city" => "Kearney"})
-    l1.insert
-    l2.insert
-    l3.insert
-    assert_equal(3, Location.all.length)
-  end
-    
-  def test_list_all_categories
-    DATABASE.execute("DELETE FROM categories")
-    
-    l1 = Category.new({"name" => "Movie"})
-    l2 = Category.new({"name" => "Book"})
-    l1.insert
-    l2.insert
-    assert_equal(2, Category.all.length)
   end
   
   def test_category_creation
     category = Category.new({"name" => "Movie"})
-    category.insert   
-    results = DATABASE.execute("SELECT name FROM categories WHERE id = #{category.id}")
+    
+    category.insert("categories")   
+    results = DATABASE.execute("SELECT name FROM categories WHERE id = 
+                                #{category.id}")
     
     added_category = results[0]
     
-    assert_equal(1, results.length)
     assert_equal("Movie", added_category["name"])
   end
+  
+  def test_all_method
+    location = Location.new({"city" => "Omaha"})
+    category = Category.new({"name" => "Book"})
+    product = Product.new({"serial_number" => 1, "description" => "Hello", 
+                            "quantity" => 5, "cost" => 10, "location_id" => 1,
+                            "category_id" => 3})
+    
+    location.insert("locations")
+    category.insert("categories")
+    product.insert("products")
+    
+    assert_equal(1, Location.all("locations").length)
+    assert_equal(1, Category.all("categories").length)
+    assert_equal(1, Product.all("products").length)
+  end
+  
+  def test_find_method
+    location = Location.new({"city" => "Omaha"})
+    category = Category.new({"name" => "Book"})
+    product = Product.new({"serial_number" => 1, "description" => "Hello", 
+                            "quantity" => 5, "cost" => 10, "location_id" => 1,
+                            "category_id" => 3})
+   
+    location.insert("locations")
+    category.insert("categories")
+    product.insert("products")
+    
+    results1 = Product.find("products", 1)
+    results2 = Location.find("locations", 1)
+    results3= Category.find("categories", 1) 
+    
+    assert_equal(10, results1.cost)
+    assert_equal("Omaha", results2.city)
+    assert_equal("Book", results3.name)                        
+  end
+
 end
 
-binding.pry
+#binding.pry
